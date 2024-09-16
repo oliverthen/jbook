eb34d64d55c00cbafd4feddba1aa7aa41c71bcca
Oliver Then <oli171dr@gmail.com>
2024-09-16 16:18:08 -0400

    Implemented formating of code editor and syntax highlighting for jsx code

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
@@ -71,7 +72,10 @@ const App = () => {
 
 	return (
 		<div>
-			<CodeEditor />
+			<CodeEditor 
+				initialValue='const a = 1;'
+				onChange={(value) => setInput(value)}
+			/>
 			<textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
 			<div>
 				<button onClick={onClick}>Submit</button>
