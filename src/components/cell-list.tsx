c052a0528aba92adf35b162b470ef606876228f6
Oliver Then <oli171dr@gmail.com>
2024-09-25 07:47:12 -0400

    Added buttons to add code or text cell and applied CSS

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from "../hooks/use-typed-selector";
-import CellListItem from "./cell-list.item";
+import AddCell from "./add-cell";
+import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
 	const cells = useTypedSelector(({cells: {order, data}}) => 
@@ -8,10 +10,18 @@ const CellList: React.FC = () => {
 		)
 	);
 
-	const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell} />)
+	const renderedCells = cells.map(cell => 
+		<Fragment key={cell.id}>
+			<AddCell nextCellId={cell.id}/>
+			<CellListItem cell={cell} />
+		</Fragment>
+	)
 
 	return (
-		<div>{renderedCells}</div>
+		<div>
+			{renderedCells}
+			<AddCell nextCellId={null}/>
+		</div>
 	);
 };
 
--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,17 @@
 import './text-editor.css';
 import MDEditor from '@uiw/react-md-editor';
 import { useEffect, useState, useRef } from 'react';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
 
-const TextEditor: React.FC = () => {
+interface TextEditorProps {
+	cell: Cell;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({cell}) => {
 	const ref = useRef<HTMLDivElement | null>(null);
 	const [editing, setEditing] = useState(false);
-	const [value, setValue] = useState('# Header');
+	const { updateCell } = useActions();
 
 	useEffect(() => {
 		const listener = (event: MouseEvent) => {
@@ -26,7 +32,7 @@ const TextEditor: React.FC = () => {
 	if (editing) {
 		return (
 			<div className='text-editor' ref={ref}>
-				<MDEditor value={value} onChange={(v) => setValue(v || '')}/>
+				<MDEditor value={cell.content} onChange={(v) => updateCell(cell.id, v || '')}/>
 			</div>
 		);
 	}
@@ -34,7 +40,7 @@ const TextEditor: React.FC = () => {
 	return (
 		<div onClick={() => setEditing(true)}>
 			<div className='card-content'>
-				<MDEditor.Markdown className='text-editor card' source={value} />
+				<MDEditor.Markdown className='text-editor card' source={cell.content || 'Click to edit'} />
 			</div>
 		</div>
 	);
