78a354e858be07704a8bd1223e759bf0a2c679e4
Oliver Then <oli171dr@gmail.com>
2024-09-26 11:08:18 -0400

    Adjusted styling of buttons and refactored Redux action of inserting new cells

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,23 +2,24 @@ import './add-cell.css';
 import { useActions } from '../hooks/use-actions';
 
 interface AddCellProps {
-	nextCellId: string | null;
+	previousCellId: string | null;
+	forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
-	const { insertCellBefore } = useActions();
+const AddCell: React.FC<AddCellProps> = ({ previousCellId, forceVisible }) => {
+	const { insertCellAfter } = useActions();
 
 
 	return (
-		<div className='add-cell'>
+		<div className={`add-cell ${forceVisible && 'force-visible'}`}>
 			<div className='add-buttons'>
-				<button className='button is-rounded is-primary is-small' onClick={() => insertCellBefore(nextCellId, 'code')}>
+				<button className='button is-rounded is-primary is-small' onClick={() => insertCellAfter(previousCellId, 'code')}>
 					<span className='icon is-small'>
 						<i className='fas fa-plus'/>
 					</span>
 					<span>Code</span>
 				</button>
-				<button className='button is-rounded is-primary is-small' onClick={() => insertCellBefore(nextCellId, 'text')}>
+				<button className='button is-rounded is-primary is-small' onClick={() => insertCellAfter(previousCellId, 'text')}>
 					<span className='icon is-small'>
 						<i className='fas fa-plus'/>
 					</span>
