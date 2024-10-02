7c6eacfbeff90e310e596d1f2a3705fd10f3cdc0
Oliver Then <oli171dr@gmail.com>
2024-10-02 19:47:42 -0400

    Setup cells router with get and post implementations

---
+++ packages/local-api/dist/routes/cells.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createCellsRouter = void 0;
+const express_1 = __importDefault(require("express"));
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
+const createCellsRouter = (filename, dir) => {
+    const router = express_1.default.Router();
+    const fullPath = path_1.default.join(dir, filename);
+    router.get('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const isLocalApiError = (err) => {
+            return typeof err.code === "string";
+        };
+        // Make sure the cell storage file exists
+        // If it does not exist, add in a default list of cells
+        // Read the file
+        // Parse a list of cells out of it
+        // Send list of cells back to browser
+    }));
+    router.post('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        // Take the list of cells from the req object
+        // serialize them
+        const { cells } = req.body;
+        // Write the cells into the file
+        yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+        res.send({ status: 'ok' });
+    }));
+    return router;
+};
+exports.createCellsRouter = createCellsRouter;
